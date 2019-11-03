35a955f2231e2c7180401d64a85357814292a170
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2019-11-04 00:27:36 +0100

    Converted code to class SeleniumScraper and made methods more general

--- scraper.py
+++ scraper.py
@@ -12,31 +12,58 @@ reddit_home = 'https://www.reddit.com'
 slash = '/r/'
 subreddit = 'MachineLearning'
 
-def setup_chrome_browser(path_chromedriver):
-    options = webdriver.ChromeOptions()
-    prefs = {"profile.default_content_setting_values.notifications" : 2}
-    options.add_experimental_option("prefs", prefs)
+class SeleniumScraper():
+    def __init__(self):
+        self.driver = None
+        self.links = []
     
-    driver = webdriver.Chrome(executable_path = path_chromedriver,
-                              options=options)
-    
-    return driver
-
-browser = setup_chrome_browser("/Users/casperbogeskovhansen/Downloads/chromedriver")
-browser.get(reddit_home + slash + subreddit)
-
-scroll_n_times = 60
-
-try:
-    while scroll_n_times:
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(0.5)
-        scroll_n_times -= 1
+    def setup_chrome_browser(self,
+                             path_chromedriver):
+        '''
+            This function allows for setting up a chrome driver for use with
+            Selenium. It expects a path to a chromedriver, available for download
+            on this link: https://chromedriver.chromium.org/home
+        '''
+        options = webdriver.ChromeOptions()
+        prefs = {"profile.default_content_setting_values.notifications" : 2}
+        options.add_experimental_option("prefs", prefs)
         
-    elements = browser.find_elements_by_xpath("//*[@data-click-id='body']")
-    links = [tag.get_attribute('href') for tag in elements]
-    
-    print(len(links))
-    
-finally:
-    browser.quit()
+        self.driver = webdriver.Chrome(executable_path = path_chromedriver,
+                                       options=options)
+        
+    def collect_links(self,
+                      page,
+                      scroll_n_times,
+                      xpath_element):
+        self.driver.get(page)
+        
+        try:
+            # When scroll_n_times = 0, loop stops
+            while scroll_n_times:
+                # Scrolls browser to the bottom of the page
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(0.5)
+                scroll_n_times -= 1
+            
+            elements = self.driver.find_elements_by_xpath(xpath_element)
+            
+            # Get the link from the href attribute
+            self.links = [tag.get_attribute('href') for tag in elements]
+            
+        finally:
+            self.driver.quit()
+        
+        return self.links
+
+SS = SeleniumScraper()
+SS.setup_chrome_browser("/Users/casperbogeskovhansen/Downloads/chromedriver")
+
+# Selects all the a elements that have a "data-click-id" attribute with a value of "body"
+xpath_element = "//a[@data-click-id='body']"
+scroll_n_times = 5
+
+links = SS.collect_links(page = reddit_home + slash + subreddit,
+                         scroll_n_times = scroll_n_times,
+                         xpath_element = xpath_element)
+
+print(len(links))
