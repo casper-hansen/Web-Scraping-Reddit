3c48e9eb224998897ed1fb425f7441f2ec32faed
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2019-11-10 23:37:24 +0100

    Added timer

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 from core.selenium_scraper import SeleniumScraper
 from core.soup_scraper import SoupScraper
 from tqdm import tqdm
+import time
 
 reddit_home = 'https://www.reddit.com'
 slash = '/r/'
 subreddit = 'MachineLearning'
 sort_by = '/hot/'
+
+start = time.time()
 
 SelScraper = SeleniumScraper()
 BSoupScraper = SoupScraper(reddit_home,
@@ -17,14 +20,14 @@ SelScraper.setup_chrome_browser()
 # Selects all the a elements that have a "data-click-id" attribute with a value of "body"
 # https://stackoverflow.com/questions/36019544/if-double-slash-is-used-2-times-in-xpath-what-does-it-mean
 xpath = "//a[@data-click-id='body']"
-scroll_n_times = 5
+scroll_n_times = 0
 
 # Collect links from subreddit
 links = SelScraper.collect_links(page = reddit_home + slash + subreddit + sort_by,
                                  scroll_n_times = scroll_n_times,
                                  xpath = xpath)
 
-print(('Caught {0} links').format(len(links)))
+print(('Caught {0} links. Getting data dicts from links now...').format(len(links)))
 
 # Find the <script> with id='data' for each link
 script_data = BSoupScraper.get_scripts(urls = links)
@@ -32,7 +35,7 @@ script_data = BSoupScraper.get_scripts(urls = links)
 # Transforms each script with data into a Python dict, returned as [{}, {}...]
 BSoupScraper.data = SelScraper.reddit_data_to_dict(script_data = script_data)
 
-for i, current_data in enumerate(tqdm(BSoupScraper.data)):
+for i, current_data in enumerate(tqdm(BSoupScraper.data, position=0, leave=True)):
     BSoupScraper.get_url_id_and_url_title(BSoupScraper.urls[i],
                                           current_data, i)
     
@@ -48,3 +51,7 @@ for i, current_data in enumerate(tqdm(BSoupScraper.data)):
     BSoupScraper.get_comment_ids()
     BSoupScraper.get_links_from_post()
     BSoupScraper.get_main_link()
+
+end = time.time()
+
+print(('\nIt took {0} seconds to scrape {1} links').format(round(end - start, 1), len(links)))
